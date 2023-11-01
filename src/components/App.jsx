2a78a71c4f890edb1092f28bf6caab48123bd63d
andrewdark <samsonov.a@ukr.net>
2023-11-01 18:32:40 +0200

    add component to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
+import React, { useEffect } from 'react';
+
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactFilter } from './ContactFilter/ContactFilter';
+import { ContactList } from './ContactList/ContactList';
+import localStorage from '../services/localStorage';
+import { useSelector, useDispatch } from 'react-redux';
+import { setContactList } from '../redux/store';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const contactsRedux = useSelector(state => state.contacts);
+
+  //componentDidMount();
+  useEffect(() => {
+    const localContacts = localStorage.load('phoneBook');
+    if (localContacts && localContacts.length > 0) {
+      dispatch(setContactList(localContacts));
+    }
+  }, [dispatch]);
+
+  //componentDidUpdate(prevProps, prevState, snapshot);
+  useEffect(() => {
+    localStorage.save('phoneBook', contactsRedux);
+  }, [contactsRedux]);
+
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        width: 400,
+        marginLeft: '8px',
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <ContactFilter />
+      <ContactList />
     </div>
   );
 };
